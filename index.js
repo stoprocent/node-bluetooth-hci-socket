bf523d838f4ea7289a42c003b6e8906873a770ac
Marek Serafin <marek@snowheads.pl>
2023-10-07 02:05:00 +0200

    Added support for HCI over UART

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var os = require('os');
 
 var platform = os.platform();
 
-if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'freebsd') {
+if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_UART || process.env.BLUETOOTH_HCI_SOCKET_UART_PORT) {
+  module.exports = require('./lib/uart.js');
+} else if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'freebsd') {
   module.exports = require('./lib/usb.js');
 } else if (platform === 'linux' || platform === 'android') {
   module.exports = require('./lib/native');
