13a54c017a16913f6f5ab6aaf1f663c7c4a49acc
Marek Serafin <marek@snowheads.pl>
2023-10-08 18:45:55 +0200

    README Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var os = require('os');
 
 var platform = os.platform();
 
-if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_UART || process.env.BLUETOOTH_HCI_SOCKET_UART_PORT) {
+if (process.env.BLUETOOTH_HCI_SOCKET_UART_PORT) {
   module.exports = require('./lib/uart.js');
 } else if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'freebsd') {
   module.exports = require('./lib/usb.js');
