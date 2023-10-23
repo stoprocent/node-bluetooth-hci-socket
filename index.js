2b2c852a8fd9396a10918ea5db56cfc614c946aa
Marek Serafin <marek@snowheads.pl>
2023-10-23 20:13:43 +0200

    fix: restored BLUETOOTH_HCI_SOCKET_FORCE_UART env handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const platform = os.platform();
 
-if (process.env.BLUETOOTH_HCI_SOCKET_UART_PORT) {
+if (process.env.BLUETOOTH_HCI_SOCKET_UART_PORT || process.env.BLUETOOTH_HCI_SOCKET_FORCE_UART) {
   module.exports = require('./lib/uart.js');
 } else if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'freebsd') {
   module.exports = require('./lib/usb.js');
