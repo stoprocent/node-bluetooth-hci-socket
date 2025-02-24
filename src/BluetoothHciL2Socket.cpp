1149918c4f8060297410dd56e059c78f59f78e69
Marek Serafin <marek@snowheads.pl>
2025-02-24 22:53:11 +0100

    fix: added missing `stdexcept` include

--- src/BluetoothHciL2Socket.cpp
+++ src/BluetoothHciL2Socket.cpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <uv.h>
+#include <stdexcept>
 
 #include "BluetoothHciL2Socket.h"
 #include "BluetoothHciSocket.h"
--- src/BluetoothHciSocket.cpp
+++ src/BluetoothHciSocket.cpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <uv.h>
+#include <stdexcept>
 
 #include "BluetoothHciSocket.h"
 
